1700769a824d1fb067c3c851bb6dbe84f6e8fb3a
Justin Chittarath <justinchitt@gmail.com>
2021-09-22 11:51:09 -0600

    commit tree 1f340c9528feae4aec6138f89cd5ec6ca27ac4fd

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function superbowlWin(array) {
+    const win = array.find(function(array) {
+        return array['result'] === 'W';
+    })
+   if (win === undefined) {
+       return undefined
+   } else {
+       return win['year'];
+   }
+}
 
